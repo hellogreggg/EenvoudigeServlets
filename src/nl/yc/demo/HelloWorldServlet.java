ba1f241451dcdb3facbf6f1e34dc2bb3eed5f6cb
hellogreggg <theawesomelifeofgreg@gmail.com>
2018-05-23 12:09:21 +0200

    init commit day 2

---
+++ src/nl/yc/demo/HelloWorldServlet.java
@@ -0,0 +1,29 @@
+package nl.yc.demo;
+
+import java.io.IOException;
+import java.time.LocalTime;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorldServlet
+ */
+@WebServlet("/HelloWorldServlet")
+public class HelloWorldServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		LocalTime time = LocalTime.now();
+		response.getWriter().print(time);
+
+	
+	}
+}
---
+++ src/nl/yc/demo/Teller.java
@@ -0,0 +1,37 @@
+package nl.yc.demo;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Teller
+ */
+@WebServlet("/Teller")
+public class Teller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	public static int teller;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Teller() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		teller++;
+		response.getWriter().print(teller);
+	
+	}
+
+
+}
